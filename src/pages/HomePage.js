15d0e316fd594f52edf4b4943b2699704314fbba
CEEJAY\quiam <quiamcoceejay77@gmail.com>
2022-12-10 00:28:01 +0800

    homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router";
-import santabg from "./santabg.jpg";
+import image from "../components/img/image.jpg";
 
 function HomePage() {
 
@@ -15,8 +15,8 @@ function HomePage() {
   }
 
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg ">
+        <div className="Image" style={{ backgroundImage:`url(${image})` }}>
+            <nav className="navbar navbar-expand-lg " style={{backgroundColor:'white'}}>
        <div className="container-fluid ">
     <a className="navbar-brand" href="#">Navbar</a>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -37,8 +37,14 @@ function HomePage() {
 </nav>
         
 
-        <div className="container-fluid bg-danger">
-         <button  type="submit" onClick={handleSubmit}>Get started</button>
+  <div className="Content" style={{border:"1px solid red"}}>
+    <p className="">Home of the world's #1 Secret Santa Generator</p>
+    <h1>Destination: Secret Santa</h1> 
+    <p>Invite your friends and family to the best way to exchange gifts. It's free, fun, and easy. You even get your own Wishlist!</p>
+      
+
+       
+         <button  type="submit" onClick={handleSubmit}>Start My secret santa</button>
         </div>
         </div>
 
