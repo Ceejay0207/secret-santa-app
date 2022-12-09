6784d7e8ab83f14d4fa23eb2eb1662804323e6be
mark2497 <markanthonysantos2497@gmail.com>
2022-12-09 23:41:36 +0800

    participant pages

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import MainPage from './pages/MainPage';
 import LoginPage from './pages/Login';
 import ParticipantList from './pages/ParticipantList';
 import Register from './pages/Register';
-import Table from './components/table';
+
+import Welcome from './pages/participant/Welcome';
+import DrawName from './pages/participant/DrawName';
+import MyWishList from './pages/participant/MyWishList';
+
 function App() {
   return (
     <div className="App">
      <Routes>
         <Route path='/' element={<HomePage/>}/>
-        <Route path='/HomePage' element={<HomePage/>}/>
         <Route path="/Login" element={<LoginPage />}/>
         <Route path='/Admin' element={<MainPage/>}/>
         <Route path="/Participantlist" element={<ParticipantList/>} />
         <Route path="/register" element={<Register/>} />
+
+        {/* Participant Routes */}
+        <Route path='/participant/' element={<Welcome/>}/>
+        <Route path='/participant/drawname' element={<DrawName/>}/>
+          <Route path='/participant/mywishlist' element={<MyWishList/>}/>
      </Routes>
     </div>
   );
